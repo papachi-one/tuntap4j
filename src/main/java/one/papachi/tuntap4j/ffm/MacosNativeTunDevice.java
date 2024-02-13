72577428ad89236ce042875d1683eecb204c60d9
papachi <pavel.csefalvay@papachi.one>
2024-02-13 03:17:17 +0100

    macos ffm initial commit

--- src/main/java/one/papachi/tuntap4j/ffm/MacosNativeTunDevice.java
+++ src/main/java/one/papachi/tuntap4j/ffm/MacosNativeTunDevice.java
@@ -1,4 +1,198 @@
 package one.papachi.tuntap4j.ffm;
 
-public class MacosNativeTunDevice {
+import one.papachi.tuntap4j.api.TunDevice;
+
+import java.io.IOException;
+import java.lang.foreign.*;
+import java.lang.invoke.VarHandle;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+
+public class MacosNativeTunDevice implements TunDevice {
+
+    public static void main(String[] args) throws Throwable {
+        String deviceName = "utun5";
+        NativeMacosApi api = new NativeMacosApi();
+        MacosNativeTunDevice tun = new MacosNativeTunDevice(api, deviceName);
+        tun.open();
+        tun.setUp(true);
+        ByteBuffer dst = ByteBuffer.allocateDirect(1514);
+        while (tun.read(dst.clear()) != -1) {
+            dst.flip();
+            System.out.println(dst);
+        }
+    }
+
+    private final NativeMacosApi api;
+
+    private final String deviceName;
+
+    private int handle;
+
+    public MacosNativeTunDevice(NativeMacosApi api, String deviceName) {
+        this.api = api;
+        this.deviceName = deviceName;
+    }
+
+    @Override
+    public boolean isOpen() throws IOException {
+        int F_GETFD = 1;
+        try {
+            return (int) api.fcntl.invokeExact(handle, F_GETFD) != -1;
+        } catch (Throwable e) {
+            throw new IOException("Native function has failed.", e);
+        }
+    }
+
+    @Override
+    public void open() throws IOException {
+        try (Arena arena = Arena.ofConfined()) {
+            int PF_SYSTEM = 32;
+            int SOCK_DGRAM = 2;
+            int SYSPROTO_CONTROL = 2;
+            String UTUN_CONTROL_NAME = "com.apple.net.utun_control";
+            int CTLIOCGINFO = (int) 3227799043L;
+            int AF_SYSTEM = 32;
+            int AF_SYS_CONTROL = 2;
+            if ((handle = (int) api.socket.invokeExact(PF_SYSTEM, SOCK_DGRAM, SYSPROTO_CONTROL)) == -1) {
+                throw new IOException("Failed to open socket.");
+            }
+            StructLayout ctl_info = MemoryLayout.structLayout(
+                    ValueLayout.JAVA_INT.withName("ctl_id"),
+                    MemoryLayout.sequenceLayout(96, ValueLayout.JAVA_BYTE).withName("ctl_name")
+            );
+            MemorySegment info = arena.allocate(100);
+            info.setString(4, UTUN_CONTROL_NAME);
+            if ((int) api.ioctl.invokeExact(handle, CTLIOCGINFO, info) == -1) {
+                throw new IOException("Failed to call ioctl.");
+            }
+            StructLayout sockaddr_ctl = MemoryLayout.structLayout(
+                    ValueLayout.JAVA_BYTE.withName("sc_len"),
+                    ValueLayout.JAVA_BYTE.withName("sc_family"),
+                    ValueLayout.JAVA_SHORT.withName("ss_sysaddr"),
+                    ValueLayout.JAVA_INT.withName("sc_id"),
+                    ValueLayout.JAVA_INT.withName("sc_unit"),
+                    ValueLayout.JAVA_INT.withName("sc_reserved[0]"),
+                    ValueLayout.JAVA_INT.withName("sc_reserved[1]"),
+                    ValueLayout.JAVA_INT.withName("sc_reserved[2]"),
+                    ValueLayout.JAVA_INT.withName("sc_reserved[3]"),
+                    ValueLayout.JAVA_INT.withName("sc_reserved[4]")
+            );
+            MemorySegment addr = arena.allocate(sockaddr_ctl);
+            addr.set(ValueLayout.JAVA_BYTE, 0, (byte) addr.byteSize());
+            addr.set(ValueLayout.JAVA_BYTE, 1, (byte) AF_SYSTEM);
+            addr.set(ValueLayout.JAVA_SHORT, 2, (short) AF_SYS_CONTROL);
+            addr.set(ValueLayout.JAVA_INT, 4, info.get(ValueLayout.JAVA_INT, 0));
+            addr.set(ValueLayout.JAVA_INT, 8, 6);// developer private number
+            ByteBuffer buffer = addr.asByteBuffer();
+            while (buffer.hasRemaining()) {
+                System.out.printf("%02x ", buffer.get());
+            }
+            System.out.println();
+            if ((int) api.connect.invokeExact(handle, addr, (int) addr.byteSize()) == -1) {
+                MemorySegment p_error = (MemorySegment) api.error.invokeExact();
+                p_error = p_error.reinterpret(4);
+                int error = p_error.get(ValueLayout.JAVA_INT, 0);
+                MemorySegment strerror = (MemorySegment) api.strerror.invokeExact(error);
+                strerror = strerror.reinterpret(1000);
+                String string = strerror.getString(0);
+                throw new IOException("Failed to call connect. " + error + " " + string);
+            }
+        } catch (IOException e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new IOException("Native function has failed.", e);
+        }
+    }
+
+    @Override
+    public int read(ByteBuffer dst) throws IOException {
+        try (Arena arena = Arena.ofConfined()) {
+            MemorySegment memorySegment = MemorySegment.ofBuffer(dst);
+            int read = (int) api.read.invokeExact(handle, memorySegment, dst.remaining());
+            dst.position(dst.position() + read);
+            return read;
+        } catch (IOException e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new IOException("Native function has failed.", e);
+        }
+    }
+
+    @Override
+    public int write(ByteBuffer src) throws IOException {
+        return 0;
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+
+    @Override
+    public boolean isUp() throws IOException {
+        return false;
+    }
+
+    @Override
+    public void setUp(boolean isUp) throws IOException {
+        try (Arena arena = Arena.ofConfined()) {
+            MemorySegment ifr = arena.allocate(32);
+            ifr.setString(0, "utun5");
+//            int handle;
+//            if ((handle = (int) api.socket.invokeExact(2, 2, 0)) == -1) {
+//                MemorySegment p_error = (MemorySegment) api.error.invokeExact();
+//                p_error = p_error.reinterpret(4);
+//                int error = p_error.get(ValueLayout.JAVA_INT, 0);
+//                MemorySegment strerror = (MemorySegment) api.strerror.invokeExact(error);
+//                strerror = strerror.reinterpret(1000);
+//                String string = strerror.getString(0);
+//                throw new IOException("Failed to call socket. " + error + " " + string);
+//            }
+//            int SIOCGIFFLAGS = (int) 3223349521L;
+//            if ((int) api.ioctl.invokeExact(handle, SIOCGIFFLAGS, ifr) == -1) {
+//                MemorySegment p_error = (MemorySegment) api.error.invokeExact();
+//                p_error = p_error.reinterpret(4);
+//                int error = p_error.get(ValueLayout.JAVA_INT, 0);
+//                MemorySegment strerror = (MemorySegment) api.strerror.invokeExact(error);
+//                strerror = strerror.reinterpret(1000);
+//                String string = strerror.getString(0);
+//                throw new IOException("Failed to call ioctl. " + error + " " + string);
+//            }
+//        } catch (IOException e) {
+//            throw e;
+        } catch (Throwable e) {
+            throw new IOException("Native function has failed.", e);
+        }
+    }
+
+    @Override
+    public InetAddress getInetAddress() throws IOException {
+        return null;
+    }
+
+    @Override
+    public void setInetAddress(InetAddress inetAddress) throws IOException {
+
+    }
+
+    @Override
+    public int getMtu() throws IOException {
+        return 0;
+    }
+
+    @Override
+    public void setMtu() throws IOException {
+
+    }
+
+    @Override
+    public byte[] getMacAddress() throws IOException {
+        return new byte[0];
+    }
+
+    @Override
+    public void setMacAddress(byte[] macAddress) throws IOException {
+
+    }
 }
--- src/main/java/one/papachi/tuntap4j/ffm/NativeMacosApi.java
+++ src/main/java/one/papachi/tuntap4j/ffm/NativeMacosApi.java
@@ -1,4 +1,156 @@
 package one.papachi.tuntap4j.ffm;
 
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.VarHandle;
+import java.nio.ByteBuffer;
+
+import static java.lang.foreign.MemoryLayout.*;
+import static java.lang.foreign.ValueLayout.*;
+
 public class NativeMacosApi {
+
+    protected Arena arena;
+    protected Linker linker;
+    protected SymbolLookup lookup;
+    protected MethodHandle error;
+    protected MethodHandle strerror;
+    protected MethodHandle fcntl;
+    protected MethodHandle open;
+    protected MethodHandle socket;
+    protected MethodHandle ioctl;
+    protected MethodHandle connect;
+    protected MethodHandle getsockopt;
+    protected MethodHandle read;
+    protected MethodHandle write;
+    protected MethodHandle close;
+    protected MethodHandle sysctl;
+    protected StructLayout in_addr;
+    protected StructLayout ifr_addr;
+    protected StructLayout ifreq;
+
+    NativeMacosApi() {
+        arena = Arena.ofShared();
+        linker = Linker.nativeLinker();
+        lookup = linker.defaultLookup();
+
+        error = linker.downcallHandle(lookup.find("__error").orElseThrow(),
+                FunctionDescriptor.of(
+                        ADDRESS.withName("return")
+                ));
+
+        strerror = linker.downcallHandle(lookup.find("strerror").orElseThrow(),
+                FunctionDescriptor.of(
+                        ADDRESS.withName("return"),
+                        JAVA_INT.withName("errnum")
+                ));
+
+        fcntl = linker.downcallHandle(lookup.find("fcntl").orElseThrow(),
+                FunctionDescriptor.of(
+                        JAVA_INT.withName("return"),
+                        JAVA_INT.withName("fildes"),
+                        JAVA_INT.withName("cmd")
+                ));
+
+        open = linker.downcallHandle(lookup.find("open").orElseThrow(),
+                FunctionDescriptor.of(
+                        JAVA_INT.withName("return"),
+                        ADDRESS.withName("path"),
+                        JAVA_INT.withName("flags")
+                ));
+
+        socket = linker.downcallHandle(lookup.find("socket").orElseThrow(),
+                FunctionDescriptor.of(
+                        JAVA_INT.withName("return"),
+                        JAVA_INT.withName("domain"),
+                        JAVA_INT.withName("type"),
+                        JAVA_INT.withName("protocol")
+                ));
+
+        ioctl = linker.downcallHandle(lookup.find("ioctl").orElseThrow(),
+                FunctionDescriptor.of(
+                        JAVA_INT.withName("return"),
+                        JAVA_INT.withName("fd"),
+                        JAVA_INT.withName("request"),
+                        ADDRESS.withName("param")
+                ));
+
+        connect = linker.downcallHandle(lookup.find("connect").orElseThrow(),
+                FunctionDescriptor.of(
+                        JAVA_INT.withName("return"),
+                        JAVA_INT.withName("sockfd"),
+                        ADDRESS.withName("addr"),
+                        JAVA_INT.withName("sizeof(addr)")// TODO
+                ));
+
+        getsockopt = linker.downcallHandle(lookup.find("getsockopt").orElseThrow(),
+                FunctionDescriptor.of(
+                        JAVA_INT.withName("return"),
+                        JAVA_INT.withName("sockfd"),
+                        JAVA_INT.withName("level"),
+                        JAVA_INT.withName("optname"),
+                        ADDRESS.withName("optval"),
+                        ADDRESS.withName("optlen")
+                ));
+
+        read = linker.downcallHandle(lookup.find("read").orElseThrow(),
+                FunctionDescriptor.of(
+                        JAVA_INT.withName("return"),
+                        JAVA_INT.withName("fd"),
+                        ADDRESS.withName("buf"),
+                        JAVA_INT.withName("count")
+                ));
+
+        write = linker.downcallHandle(lookup.find("write").orElseThrow(),
+                FunctionDescriptor.of(
+                        JAVA_INT.withName("return"),
+                        JAVA_INT.withName("fd"),
+                        ADDRESS.withName("buf"),
+                        JAVA_INT.withName("count")
+                ));
+
+        close = linker.downcallHandle(lookup.find("close").orElseThrow(),
+                FunctionDescriptor.of(
+                        JAVA_INT.withName("return"),
+                        JAVA_INT.withName("fd")
+                ));
+
+        sysctl = linker.downcallHandle(lookup.find("sysctl").orElseThrow(),
+                FunctionDescriptor.of(
+                        JAVA_INT.withName("return"),
+                        ADDRESS.withName("name"),
+                        JAVA_INT.withName("namelen"),
+                        ADDRESS.withName("oldp"),
+                        ADDRESS.withName("oldlenp"),
+                        ADDRESS.withName("newp"),
+                        JAVA_INT.withName("newlen")
+                ));
+
+        in_addr = structLayout(
+                JAVA_INT.withName("s_addr")
+        ).withName("sin_addr");
+
+        ifr_addr = structLayout(
+                JAVA_BYTE.withName("sin_len"),
+                JAVA_BYTE.withName("sin_family"),
+                JAVA_SHORT.withName("sin_port"),
+                in_addr,
+                sequenceLayout(8, JAVA_BYTE).withName("sin_zero")
+        ).withName("ifr_addr");
+
+        ifreq = structLayout(
+                sequenceLayout(16, JAVA_BYTE).withName("ifr_name"),
+                unionLayout(
+                        JAVA_SHORT.withName("ifru_flags"),
+                        JAVA_INT.withName("ifru_mtu"),
+                        ifr_addr
+                ).withName("if_ifru")
+        ).withName("ifreq");
+
+    }
+
+    public static void main(String[] args) {
+        NativeMacosApi api = new NativeMacosApi();
+    }
+
 }
